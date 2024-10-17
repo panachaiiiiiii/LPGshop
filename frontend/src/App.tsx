1788262de7bf7f7e5a188e7f3532df537c79351d
panachaiiiiiii <112429933+panachaiiiiiii@users.noreply.github.com>
2024-10-17 16:21:47 +0700

    s

---
+++ frontend/src/App.tsx
@@ -0,0 +1,59 @@
+import {
+  AppstoreOutlined,
+  MailOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Menu } from "antd";
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AddItemPage from "./page/Additem_page";
+import HomePage from "./page/home";
+import SettingPage from "./page/setting_page";
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+const items: MenuItem[] = [
+  {
+    label: <a href="/">ขายสินค้า</a>,
+    key: "sell",
+    icon: <MailOutlined />,
+  },
+  {
+    label: <a href="/add-item">ยอดรวม</a>,
+    key: "app",
+    icon: <AppstoreOutlined />,
+  },
+  {
+    label: <a href="/add-item">คลังสินค้า</a>,
+    key: "app",
+    icon: <AppstoreOutlined />,
+  },
+  
+];
+function App() {
+  const [current, setCurrent] = useState("mail");
+
+  const onClick: MenuProps["onClick"] = (e) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  };
+  return (
+    <BrowserRouter>
+      <Menu
+        onClick={onClick}
+        selectedKeys={[current]}
+        mode="horizontal"
+        items={items}
+        className="mb-10"
+      />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="setting" element={<SettingPage />} />
+        <Route path="add-item" element={<AddItemPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ server/server.tsx
@@ -0,0 +1,4 @@
+import express from "express";
+const app = express;
+
+app.listen(3000, () => console.log("server is runing port 3000" ));
